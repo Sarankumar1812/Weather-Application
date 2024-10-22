e7cafe1d07e88c9c4ce2f4c32d1ef9b4b8358238
Sarankumar1812 <iam.sharanyv@gmail.com>
2024-10-22 14:05:27 +0530

    final complrtion of project

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Bengaluru",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+  {
+    name: "Chennai",
+    country: "IN",
+    state: "Tamil Nadu",
+    lat: 13.0900, 
+    lon: 80.2700
+  },
+  {
+    name: "Pune",
+    country: "IN",
+    state: "Maharashtra",
+    lat: 18.5204,
+    lon: 73.8567,
+  },
+  {
+    name: "Dubai",
+    country: "UAE",
+    state: "Ontario",
+    lat: 25.276987, 
+    lon: 55.296249, 
+  },
+  {
+    name: "Tokyo",
+    country: "JP",
+    state: "Tokyo Metropolis",
+    lat: 35.652832, 
+    lon: 139.839478, 
+  },
+];
+
+export default defaultStates;
